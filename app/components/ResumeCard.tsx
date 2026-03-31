b91debb024e9e029e41998f690326c98731c83db
AjinkyaYdande <ajinkyavaydande57@gmail.com>
2026-04-01 00:04:59 +0530

    Bug fixing and styling

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,17 +1,27 @@
 import {Link} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
 
-const ResumeCard = ({resume:{id,companyName,jobTitle,feedback}}: { resume: Resume }) => {
+const ResumeCard = ({resume: {id, companyName, jobTitle, feedback, imagePath}}: { resume: Resume }) => {
+    console.log(imagePath, "resume");
     return (
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
-            <div className="flex flex-col gap-2">
-                <h2 className="!text-black font-bold break-word">
-                    {companyName}
-                </h2>
-                <h3 className="text-lg wrap-break-word" >
-                    {jobTitle}
-                </h3>
-                <ScoreCircle score={feedback.ATS.score}/>
+            <div className="resume-card-header">
+                <div className="flex flex-col gap-2">
+                    <h2 className="!text-black font-bold break-word">{companyName}</h2>
+                    <h3 className="text-lg wrap-break-word">{jobTitle}</h3>
+                </div>
+                <div className="shrink-0">
+                    <ScoreCircle score={feedback.overallScore}/>
+                </div>
+            </div>
+            <div className="gradient-border animate-in fade-in duration-1000">
+                <div className="w-full h-full">
+                    <img
+                        src={imagePath}
+                        alt="resume"
+                        className="w-full h-75.5 max-sm:h-50 object-cover object-top"
+                    />
+                </div>
             </div>
         </Link>
     )
